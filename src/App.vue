e7ae218ea66129d1005272a3f777a8e9fdf89bd4
Melvin <melvin.courant@lasuiteandco.com>
2025-09-25 10:51:35 +0200

    :tada: init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import "./assets/css/main.scss";
+import "./assets/css/components/app.scss";
+import Header from "./components/layouts/Header.vue";
+</script>
+
+<template>
+  <main class="main">
+    <Header/>
+    <h1>Winmac OS</h1>
+  </main>
+</template>
