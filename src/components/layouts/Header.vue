177d8da66d2b892ed0f7247dcf2c49ec9c7d4c43
Melvin <melvin.courant@lasuiteandco.com>
2025-09-26 12:21:26 +0200

    :lipstick: hide header right when all items are unactive and put a height to header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -11,36 +11,52 @@ defineProps({
 
 <template>
   <header class="header">
-    <div v-for="group in header" :class="`header__${group.side}`">
-      <template v-for="item in group.items">
-        <div v-if="item.active && (!item.subitems || item.subitems.some(s => s.active))" class="header__item">
-          <button v-if="item.subitems" class="header__subitem">
-            <template v-for="(subitem, index) in item.subitems">
-              <p v-if="subitem.active">
-                <template v-if="subitem.value < 10">0</template
-                >{{ subitem.value
-                }}<span
-                  v-if="
-                    index < item.subitems.filter((s) => s.active).length - 1
-                  "
-                  class="header__subitem-divider"
-                >
-                  <template v-if="item.name === 'date'">/</template>
-                  <template v-else>:</template>
-                </span>
-              </p>
-            </template>
-          </button>
-          <button
-            v-else
-            class="header__item-button"
-            :style="{ color: item.color ? `var(--${item.color})` : '' }"
+    <template v-for="group in header">
+      <div
+        :class="`header__${group.side}`"
+        v-show="
+          group.side === 'left' ||
+          (group.side === 'right' && group.items.some((item) => item.active && (!item.subitems || item.subitems.some((s) => s.active))))
+        "
+      >
+        <template v-for="item in group.items">
+          <div
+            v-if="
+              item.active &&
+              (!item.subitems || item.subitems.some((s) => s.active))
+            "
+            class="header__item"
           >
-            <div v-if="item.icon" v-html="item.icon"></div>
-            <template v-else>{{ item.value }}</template>
-          </button>
-        </div>
-      </template>
-    </div>
+            <button v-if="item.subitems" class="header__subitem">
+              <template v-for="(subitem, index) in item.subitems">
+                <p v-if="subitem.active">
+                  <template v-if="subitem.value < 10">0</template
+                  >{{ subitem.value
+                  }}<span
+                    v-if="
+                      index <
+                      item.subitems.filter((subitem) => subitem.active).length -
+                        1
+                    "
+                    class="header__subitem-divider"
+                  >
+                    <template v-if="item.name === 'date'">/</template>
+                    <template v-else>:</template>
+                  </span>
+                </p>
+              </template>
+            </button>
+            <button
+              v-else
+              class="header__item-button"
+              :style="{ color: item.color ? `var(--${item.color})` : '' }"
+            >
+              <div v-if="item.icon" v-html="item.icon"></div>
+              <template v-else>{{ item.value }}</template>
+            </button>
+          </div>
+        </template>
+      </div>
+    </template>
   </header>
 </template>
