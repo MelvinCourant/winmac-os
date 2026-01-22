b126cd2364d27a222205fe62f350800159ec2825
Melvin <melvin.courant@lasuiteandco.com>
2026-01-22 11:36:14 +0100

    :sparkles: Create clock app and add tabs

---
+++ src/components/apps/clock/Clock.vue
@@ -0,0 +1,43 @@
+<script setup>
+import '../../../assets/css/components/apps/clock/_clock.scss';
+import { inject, onMounted, reactive } from 'vue';
+import Tabs from './Tabs.vue';
+
+defineProps({
+  app: {
+    type: Object,
+    required: true,
+  },
+});
+
+const onAppMounted = inject('onAppMounted');
+onMounted(() => {
+  if (onAppMounted) {
+    onAppMounted('Clock');
+  }
+});
+
+const tabs = reactive([
+  {
+    name: 'timedate',
+    title: 'Horloge',
+    active: true,
+  },
+  {
+    name: 'stopwatch',
+    title: 'Chronomètre',
+    active: false,
+  },
+  {
+    name: 'timer',
+    title: 'Minuterie',
+    active: false,
+  },
+]);
+</script>
+
+<template>
+  <div class="clock">
+    <Tabs :tabs="tabs" />
+  </div>
+</template>
---
+++ src/components/apps/clock/Tabs.vue
@@ -0,0 +1,21 @@
+<script setup>
+import '../../../assets/css/components/apps/clock/_tabs.scss';
+
+defineProps({
+  tabs: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="tabs">
+    <input
+      :class="['tab', { 'tab--active': tab.active }]"
+      type="button"
+      v-for="tab in tabs"
+      :value="tab.title"
+    />
+  </div>
+</template>
